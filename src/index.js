8d55e865c6e8aeac3191b4b38422e47bfd6f6aad
Mel Ashby <mel@lemonydesign.co.uk>
2020-06-22 11:18:51 +0100

    Functional refactor

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ document.getElementById("app").innerHTML = `
 `;
 
 /// Functional programming
+
 // Immutable vs mutable
 // Use immutable - don't update variables too often
 // pure function - always returns the same thing, with the same input = do one thing and do it the same each time
@@ -36,3 +37,55 @@ const addFour = x => {
 console.log(addFour(2));
 console.log(addFour(2));
 console.log(addFour(2));
+
+// Functional refactor
+// not so great - not testable
+// subtotal correct?
+// total correct?
+// email sent?
+
+const cart = [10, 5, 15];
+const fakeAPICharge = total => true;
+const fakeSendReceipt = total => true;
+
+const badCheckout = cart => {
+  let total = cart.reduce((tempTotal, item) => tempTotal + item);
+  total = total + 10;
+  const orderSuccess = fakeAPICharge(total);
+  if (orderSuccess) {
+    fakeSendReceipt({
+      email: "fakeemail@gmail.com",
+      total
+    });
+  }
+  return orderSuccess;
+};
+
+// refactor
+
+const SHIPPING_COST = 10;
+
+// smaller responsibility functions
+// can now test on subTotal and total by extrapolating logic away from checkout
+// reusables
+const getSubTotal = cart => cart.reduce((tempTotal, item) => tempTotal + item);
+const getTotal = subTotal => subTotal + SHIPPING_COST;
+const sendReceipt = ({ email, total }) =>
+  fakeSendReceipt({
+    email,
+    total
+  });
+
+const checkout = cart => {
+  const subTotal = getSubTotal(cart);
+  console.log(subTotal);
+  const total = getTotal(subTotal);
+  console.log(total);
+  const orderSuccess = fakeAPICharge(total);
+  if (orderSuccess) {
+    sendReceipt({ email: "fakeemail@gmail.com", total });
+  }
+  return orderSuccess;
+};
+
+console.log(checkout(cart));
